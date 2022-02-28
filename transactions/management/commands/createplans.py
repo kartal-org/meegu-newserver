c15f264b04d470c3e69ac594fc955968e89448b2
nathanmaru <nathanmaru7@gmail.com>
2022-02-28 18:50:43 +0800

    update createplan command

--- transactions/management/commands/createplans.py
+++ transactions/management/commands/createplans.py
@@ -1,32 +1,27 @@
 from django.core.management.base import BaseCommand
-from faker import Faker
-import faker.providers
 from transactions.models import SubscriptionPlan
-import random
-import decimal
-from django.contrib.auth import authenticate, login
- 
+
+
 class Command(BaseCommand):
     help = "Command Information"
 
     def handle(self, *args, **kwargs):
 
-        fake = Faker(["tl_PH"])  
         SubscriptionPlan.objects.create(
-            name="Institution Basic", 
-            description="Free One Time Subscription Plan For Institution.", 
-            price=50.00,
-            storageLimit=10000000000,
+            name="Institution Basic",
+            description="Get your Free 5GB of storage for your institution's publications and resources",
+            price=0.00,
+            storageLimit=5000000000,
         )
         SubscriptionPlan.objects.create(
             name="Institution Upgrade 1",
-            description="Extend 5GB to your Institution Storage",
+            description="Get another 5GB of storage for your institution's publications and resources",
             price=50.00,
-            storageLimit=10000000000,
+            storageLimit=5000000000,
         )
         SubscriptionPlan.objects.create(
             name="Institution Upgrade 2",
-            description="Extend 5GB to your Institution Storage",
-            price=50.00, 
-            storageLimit=10000000000,
-        ) 
+            description="Get another 15GB of storage for your institution's publications and resources",
+            price=100.00,
+            storageLimit=15000000000,
+        )
