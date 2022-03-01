b5946a6109f9018f1b87df60846d187794d1819d
ArkAsylum <neekol.alvarez@gmail.com>
2022-03-01 17:35:12 +0800

    neek

--- core/settings.py
+++ core/settings.py
@@ -184,7 +184,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001", "https://meegu-newserver.herokuapp.com/"]
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
